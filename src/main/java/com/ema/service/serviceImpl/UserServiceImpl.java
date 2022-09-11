11d0ed4be13c5201ade600ae65709e7b6e097118
obemeuche <obemeuchechi@gmail.com>
2022-09-11 20:09:57 +0100

    continued implementation

--- src/main/java/com/ema/service/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/ema/service/serviceImpl/UserServiceImpl.java
@@ -1,10 +1,19 @@
 package com.ema.service.serviceImpl;
 
 import com.ema.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+@Slf4j
 public class UserServiceImpl implements UserService, UserDetailsService {
 
     @Override
